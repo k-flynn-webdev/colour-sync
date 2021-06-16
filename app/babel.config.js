22eb06279ba189455d5a7bd23ad51d38c873a74d
kevin flynn <flynny85@gmail.com>
2021-06-16 19:52:58 +0100

    webpack start

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,5 +1,16 @@
+// module.exports = {
+//   presets: [
+//     '@vue/app'
+//   ]
+// }
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
-}
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
+    ],
+  ],
+};
