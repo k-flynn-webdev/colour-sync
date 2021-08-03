dceab05a014e119beb16c4ecd1ce97167c306536
kevin flynn <flynny85@gmail.com>
2021-08-03 23:30:21 +0100

    add: db backup file

--- env_prod/db_backup.sh
+++ env_prod/db_backup.sh
@@ -7,4 +7,4 @@ dir_project=$dir_project_production/..;
 ## VARS
 source $dir_project/.env
 
-mysqldump -u $DATABASE_USER -p $DATABASE_PASS $DATABASE_NAME | gzip -9 > $(date +%y%m%d)_dbBackUpsql.gz
+mysqldump -u $DATABASE_USER -h 127.0.0.1 -p $DATABASE_PASS $DATABASE_NAME | gzip -9 > minitrack_$(date +%y%m%d)_DB.gz
