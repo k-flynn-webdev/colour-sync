21044a60235737a4ff0dcbe0b277dd1d1c06634a
kevin flynn <flynny85@gmail.com>
2021-08-04 19:52:39 +0100

    fix: naming of file

--- env_prod/db_backup.sh
+++ env_prod/db_backup.sh
@@ -7,4 +7,4 @@ dir_project=$dir_project_production/..;
 ## VARS
 source $dir_project/.env
 
-mysqldump -u $DATABASE_USER -h 127.0.0.1 -p $DATABASE_PASS $DATABASE_NAME | gzip -9 > minitrack_$(date +%y%m%d)_DB.gz
+mysqldump -u $DATABASE_USER -h 127.0.0.1 -p $DATABASE_NAME | gzip -9 > ../../$APP_NAME_SHORT$(date +_%y_%m_%d)_DB.gz
